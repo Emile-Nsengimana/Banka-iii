7a6ed69594a0c4da07851751e6b8822aa4c6f6af
Emile Nsengimana <emilereas7@gmail.com>
2019-05-14 09:39:17 +0200

    allow resource sharing through cors

--- src/server.js
+++ src/server.js
@@ -1,9 +1,15 @@
 import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 import routes from './router/router';
 
 const app = express();
-app.use(express.json());
-app.use(routes);
+app
+  .use(cors())
+  .use(bodyParser.json())
+  .use(express.urlencoded({ extended: false }))
+  .use(routes);
+
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
